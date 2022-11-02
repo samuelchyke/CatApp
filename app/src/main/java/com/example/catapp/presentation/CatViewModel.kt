7bd1f13ea46a7d3ca963763ade5b55a38a0a5ebd
samuel <samuelchyke@hotmail.com>
2022-11-02 10:35:42 +0000

    Set Up ViewModel

---
+++ app/src/main/java/com/example/catapp/presentation/CatViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.catapp.presentation
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.catapp.model.CatItem
+import com.example.catapp.model.CatResponse
+import com.example.catapp.repository.NetworkRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CatViewModel @Inject constructor(
+    private val networkRepository: NetworkRepository
+) : ViewModel() {
+
+    val cat: MutableState<List<CatItem>> = mutableStateOf(listOf())
+
+    val limit = mutableStateOf(10)
+
+    init {
+        searchCats()
+    }
+
+    fun searchCats() = viewModelScope.launch {
+        val response = networkRepository.getListOfCats(limit.value)
+        response.body()?.let {
+            cat.value = it
+        }
+    }
+
+}
+
